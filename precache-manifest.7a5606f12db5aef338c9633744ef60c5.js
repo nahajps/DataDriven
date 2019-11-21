37aeda415af902b63d8d99b61cc28504f440fc44
nahajps <50195985+nahajps@users.noreply.github.com>
2019-11-21 11:54:17 +0530

    Add files via upload

---
+++ precache-manifest.7a5606f12db5aef338c9633744ef60c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcdf6c8bf11a98d83853d9e2df540458",
+    "url": "/ddp/index.html"
+  },
+  {
+    "revision": "7cc18e66b75aac49cacb",
+    "url": "/ddp/static/css/main.4301a9d4.chunk.css"
+  },
+  {
+    "revision": "13f4e7d814d15556deec",
+    "url": "/ddp/static/js/2.ca9d78b0.chunk.js"
+  },
+  {
+    "revision": "7cc18e66b75aac49cacb",
+    "url": "/ddp/static/js/main.422fbc81.chunk.js"
+  },
+  {
+    "revision": "a8931f4321a28de4d1f3",
+    "url": "/ddp/static/js/runtime~main.8d8f4589.js"
+  }
+]);
